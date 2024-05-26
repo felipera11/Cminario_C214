5b2c9ad973ae35291c8470a6f47fe5789c79385e
felipera11 <felipe.s@ges.inatel.br>
2024-05-26 14:08:18 -0300

    feat: new calculator class for testing

---
+++ src/calculator.py
@@ -0,0 +1,17 @@
+Class Calculator:
+    def __init__(self):
+        pass
+
+    def sum(self, a, b):
+        return a + b
+
+    def sub(self, a, b):
+        return a - b
+
+    def mul(self, a, b):
+        return a * b
+
+    def div(self, a, b):
+        if b == 0:
+            raise ValueError("Division by zero")
+        return a / b
